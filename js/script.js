5eeac32079ac18babbd53ef2fee41bafdfd60871
azizobaidnl-rgb <azizobaidnl@gmail.com>
2026-07-16 19:24:24 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("AziBal website loaded successfully");
 
 });
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+if(menuToggle){
+
+menuToggle.addEventListener("click", function(){
+
+navMenu.classList.toggle("active");
+
+});
+
+}
